84caf1ef63747cde86127c6e8b6d122e7c8e19c3
Leonardo Oltramari <oltramari515@gmail.com>
2024-06-06 16:14:04 -0300

    cs50

---
+++ Movies/4.sql
@@ -0,0 +1,9 @@
+SELECT COUNT(title)
+FROM movies
+JOIN ratings
+ON ratings.movie_id = movies.id
+WHERE rating = 10.0;SELECT COUNT(title)
+FROM movies
+JOIN ratings
+ON ratings.movie_id = movies.id
+WHERE rating = 10.0;
